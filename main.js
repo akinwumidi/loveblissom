927628998d7910745b46fe86ebf49aa57bc16672
Akinwumidi <israelakin14@gmail.com>
2024-08-30 14:52:06 +0100

    core update: implement navigation guide

--- main.js
+++ main.js
@@ -25,19 +25,5 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
-    // breakpoints: {
-    //     640: {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //     },
-    //     768: {
-    //         slidesPerView: 3,
-    //         spaceBetween: 30,
-    //     },
-    //     1024: {
-    //         slidesPerView: 4,
-    //         spaceBetween: 20,
-    //     },
-    // },
 });
 // Animated Hamburger menu ends here
